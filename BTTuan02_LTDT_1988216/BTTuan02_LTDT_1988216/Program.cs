d1f04258a06b2ff66c48f14c989b9258e7fc2226
unknown <tong.congdanh@gmail.com>
2021-10-11 00:35:17 +0700

    add bt2

---
+++ BTTuan02_LTDT_1988216/BTTuan02_LTDT_1988216/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTTuan02_LTDT_1988216
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
